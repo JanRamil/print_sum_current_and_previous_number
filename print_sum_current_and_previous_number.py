2c39ee80c7e0b4df3a4464d6e50b392a023e6184
JAN RAMIL ELIZER ALARCON <janramila@gmail.com>
2024-01-20 23:21:08 +0800

    Creating a loop

--- print_sum_current_and_previous_number.py
+++ print_sum_current_and_previous_number.py
@@ -1,1 +1,18 @@
 # Print the sum of the current number and the previous number
+
+#pseudocode
+
+# Creating a starting point
+print("Printing current and previous number and their sum in a range(10)")
+
+# Creating a variable or name called previous_number and set to zero
+previous_number = 0
+
+# Loop from 1 to 10
+for i in range(1,11):
+    # Variable or name called previous_number and set to zero
+    current_number = i
+    # Variable called sum_of_previous_and_current_number and set it to previous_number + current_number
+    sum_of_previous_and_current_number = previous_number + current_number
+
+
